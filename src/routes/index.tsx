5d9f35f4588cdf95a6071626314e8cafafe98823
Jhonathan Amaral <jhon14amaral1@gmail.com>
2026-04-06 19:56:33 -0300

    refactor: restructure main route configuration

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,73 @@
-import { Route, Routes } from "react-router";
-import PublicRoutes from "./public-routes";
-import PrivateRoutes from "./private-routes";
+import { createBrowserRouter } from "react-router";
+import { ProtectRoute, PublicOnlyRoute } from "@/routes/routes-layout";
 
-export default function AppRoutes() {
-  // ← Sem prop
-  return (
-    <Routes>
-      <Route path="/*" element={<PublicRoutes />} />
-      <Route path="/app/*" element={<PrivateRoutes />} />
-    </Routes>
-  );
-}
+// layouts
+import { PublicLayout } from "@/layouts/public-layout";
+import { PrivateLayout } from "@/layouts/private-layout";
+
+// páginas públicas
+import { LoginPage } from "@/feature/auth/pages/login-page";
+
+// páginas privadas
+import StudentDashboard from "@/feature/student-dashboard/pages";
+import NotFoundPage from "@/pages/not-found";
+import { RegisterPage } from "@/feature/auth/pages/register-page";
+
+import { AdminLoginPage } from "@/feature/auth/pages/admin-login";
+import { Home } from "@/feature/landing-page/page";
+import CoursesPage from "@/feature/courses/pages";
+import VideoLesson from "@/feature/video-lesson/page";
+import AdminDashboard from "@/components/admin";
+import { routes } from "@/types/routes-front";
+//import { AulasPage } from "@/pages/aulas";
+
+export const router = createBrowserRouter([
+  // ─── rotas públicas ────────────────────────────────
+  {
+    element: <PublicOnlyRoute />,
+    children: [
+      {
+        element: <PublicLayout />,
+        children: [
+          { path: routes.public.home, element: <Home /> },
+          { path: routes.public.login, element: <LoginPage /> },
+          { path: routes.public.register, element: <RegisterPage /> },
+          { path: routes.public.adminLogin, element: <AdminLoginPage /> },
+        ],
+      },
+    ],
+  },
+
+  // ─── rotas privadas ────────────────────────────────
+  {
+    element: <ProtectRoute />, // bloqueia se não logado
+    children: [
+      {
+        element: <PrivateLayout roles={["aluno"]} />,
+        children: [
+          { path: routes.student.dashboard, element: <StudentDashboard /> },
+          { path: routes.student.courses, element: <CoursesPage /> },
+          { path: routes.student.courseDetail, element: <VideoLesson /> },
+        ],
+      },
+      {
+        element: <PrivateLayout roles={["admin"]} />,
+        children: [
+          { path: routes.admin.dashboard, element: <AdminDashboard /> },
+          //{ path: routes.admin.courses, element: <CoursesPage /> },
+        ],
+      },
+      {
+        element: <PrivateLayout roles={["professor"]} />,
+        children: [
+          { path: routes.teacher.dashboard, element: <StudentDashboard /> },
+          //{ path: routes.teacher.courses, element: <CoursesPage /> },
+        ],
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
+]);
