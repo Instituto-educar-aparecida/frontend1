97716bb2318f3082912cc10d4ad16722a3b2a039
Jhonathan Amaral <jhon14amaral1@gmail.com>
2026-04-06 19:37:26 -0300

    refactor(button): The button's data interface has been refactored.

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,9 +5,10 @@ interface IButton {
   icon: IconType;
 }
 
-export function Button({ title, icon: Icon }: IButton) {
+export function Button({ title, icon: Icon, ...props }: IButton) {
   return (
     <button
+      {...props}
       type="submit"
       className="w-full py-3 bg-violet-400 hover:bg-violet-500 disabled:opacity-50 text-white font-semibold rounded-xl shadow-lg shadow-violet-500/40 transition-all duration-200 flex items-center justify-center gap-2 text-sm cursor-pointer"
     >
