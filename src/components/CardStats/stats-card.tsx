1d67b8efc0e31cca569fba7f7b020f6ad011b0ee
Jhonathan Amaral <jhon14amaral1@gmail.com>
2026-05-16 21:07:30 -0300

    feat: update components card

--- src/components/CardStats/stats-card.tsx
+++ src/components/CardStats/stats-card.tsx
@@ -1,11 +1,10 @@
-import { MdPlayCircleOutline } from "react-icons/md";
 import ContainerCard from "./container-card";
 import IconCard from "./icon-card";
 import type { IconType } from "react-icons/lib";
 
 export interface IStatsCard {
   className: string;
-  status?: string;
+  status: string;
   icon: IconType;
   title?: string;
   description?: string;
@@ -19,12 +18,10 @@ export function StatsCard({
   title,
 }: IStatsCard) {
   return (
-    <ContainerCard className="p-6 md:p-4 lg:p-6 flex-1 max-w-76 gap-4 ">
-      <IconCard icon={Icon} className={`${className}`} status={status} />
-      <div className="flex flex-col md:gap-2">
-        <span className="text-xl md:text-sm lg:text-xl  font-bold text-gray-100">
-          {description}
-        </span>
+    <ContainerCard className="p-6 w-full flex-1 max-w-80 gap-4 ">
+      <IconCard icon={Icon} className={className} status={status} />
+      <div className="flex flex-col">
+        <span className="text-xl font-bold text-gray-100">{description}</span>
         <span className="text-sm text-gray-300">{title}</span>
       </div>
     </ContainerCard>
